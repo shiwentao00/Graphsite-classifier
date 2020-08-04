6514b7962e94d6d8d3a4fd9191b1b9ac3f7026cd
Wentao Shi <wentao771@gmail.com>
2020-08-04 15:56:06 -0500

    pairs of train, validation, and test finshed.

--- dataloader.py
+++ dataloader.py
@@ -2,6 +2,7 @@
 Dataloader for the Siamese graph neural network. 
 """
 import random
+import itertools
 import torch
 from torch_geometric.data import Data, Dataset
 from torch_geometric.data import DataLoader
@@ -43,9 +44,9 @@ class PairDataset(Dataset):
     def __gen_neg_pairs()
 '''
 
-def divide_and_gen_pairs(cluster_file_dir, num_classes, th):
+def divide_and_gen_pairs(cluster_file_dir, num_classes, cluster_th):
     """
-    Generate pairs of pockets for train, validation, and test.
+    Divide the dataset and generate pairs of pockets for train, validation, and test.
 
     Arguments:
         cluster_file_dir: directory of the cluster file.
@@ -56,18 +57,21 @@ def divide_and_gen_pairs(cluster_file_dir, num_classes, th):
     clusters = read_cluster_file(cluster_file_dir)
 
     # select clusters according to rank of sizes and sample large clusters
-    clusters = select_classes(clusters, num_classes, th)
+    clusters = select_classes(clusters, num_classes, cluster_th)
 
     # divide the clusters into train, validation and test
     train_clusters, val_clusters, test_clusters = divide_clusters(clusters)
 
     # train pairs
+    train_pos_pairs, train_neg_pairs = gen_pairs(clusters=train_clusters, pos_pair_th=1200, neg_pair_th=25)
 
     # validation pairs
+    val_pos_pairs, val_neg_pairs = gen_pairs(clusters=val_clusters, pos_pair_th=1000, neg_pair_th=10)
 
     # test pairs
+    test_pos_pairs, test_neg_pairs = gen_pairs(clusters=test_clusters, pos_pair_th=1000, neg_pair_th=10)
 
-    
+    return train_pos_pairs, train_neg_pairs, val_pos_pairs, val_neg_pairs, test_pos_pairs, test_neg_pairs
 
 
 def read_cluster_file(cluster_file_dir):
@@ -111,14 +115,14 @@ def select_classes(clusters, num_classes, th):
     return selected_classes
     
 
-def sample_from_list(cluster, th):
+def sample_from_list(input_list, th):
     """
     Randomly samples th pockets if number of pockets larger than th. 
     """
-    if len(cluster) > th:
-        return random.sample(cluster, th)
+    if len(input_list) > th:
+        return random.sample(input_list, th)
     else:
-        return cluster
+        return input_list
 
 
 def divide_clusters(clusters):
@@ -153,11 +157,43 @@ def divide_clusters(clusters):
     return train_clusters, val_clusters, test_clusters
 
 
+def gen_pairs(clusters, pos_pair_th=1000, neg_pair_th=20):
+    """
+    Generate pairs of pockets from input clusters.
+    """
+    # generate pairs of pockets in the same cluster
+    pos_pairs = []
+    for cluster in clusters:
+        pairs = list(itertools.combinations(cluster, 2))
+        pairs = sample_from_list(pairs, pos_pair_th)
+        pos_pairs.extend(pairs)
+        
+    # generate pairs of pockets in different clusters
+    neg_pairs = []
+    class_pairs = list(itertools.combinations(clusters, 2))
+    for class_pair in class_pairs:
+        pairs = [(x, y) for x in class_pair[0] for y in class_pair[1]] 
+        pairs = sample_from_list(pairs, neg_pair_th)
+        neg_pairs.extend(pairs)
+    
+    return pos_pairs, neg_pairs
+
+
 if __name__=="__main__":
     cluster_file_dir = '../data/googlenet-classes'
+    num_classes = 150
+    cluster_th = 400
     #dataset = PairDataset(cluster_file_dir=cluster_file_dir, num_classes=300, th=800)
-    divide_and_gen_pairs(cluster_file_dir=cluster_file_dir, num_classes=150, th=800)
-
+    train_pos_pairs, train_neg_pairs, val_pos_pairs, val_neg_pairs, test_pos_pairs, test_neg_pairs = divide_and_gen_pairs(cluster_file_dir=cluster_file_dir, num_classes=num_classes, cluster_th=cluster_th)
+    
+    print('number of classes:', num_classes)
+    print('max number of data of each class:', cluster_th)
+    print('number of train positive pairs:', len(train_pos_pairs))
+    print('number of train negative pairs:', len(train_neg_pairs))
+    print('number of validation positive pairs:', len(val_pos_pairs))
+    print('number of validation negative pairs:', len(val_neg_pairs))
+    print('number of test positive pairs:', len(test_pos_pairs))
+    print('number of test negative pairs:', len(test_neg_pairs))
 
 
     #data = PairData(x_a, edge_index_a, edge_attr_a, x_b, edge_index_b, edge_attr_b)
