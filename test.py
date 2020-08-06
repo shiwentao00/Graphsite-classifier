7dfc3443d57e8ec2b632732e3bdef81608bb224e
Wentao Shi <wentao771@gmail.com>
2020-08-06 17:14:12 -0500

    working on multi gpu

---
+++ test.py
@@ -0,0 +1,7 @@
+import torch 
+
+#device = torch.device('cuda' if torch.cuda.is_available() else 'cpu') # detect cpu or gpu
+#print('device: ', device)
+if torch.cuda.is_available():
+    num_gpu = torch.cuda.device_count()
+    print('number of gpus: ', num_gpu)
