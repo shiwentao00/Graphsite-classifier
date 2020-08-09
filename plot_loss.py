5bed22954a113d718d553ccecb074d95ea8e1e15
Wentao Shi <wentao771@gmail.com>
2020-08-09 13:48:20 -0500

    loss function plot finished.

--- plot_loss.py
+++ plot_loss.py
@@ -1,32 +1,58 @@
 import json
+import argparse
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
 
+def get_args():
+    parser = argparse.ArgumentParser('python')
+
+    parser.add_argument('-run',
+                        required=True,
+                        type=int,
+                        help='which experiment.')
+
+    return parser.parse_args()
 
 def plot_loss(train_loss, val_loss, loss_dir, time):
     """
     Plot loss.
     """
+    font = {'size'   : 14}
+    matplotlib.rc('font', **font)   
+
     epochs = np.array(range(time), dtype=int) + 1
-    fig = plt.figure(figsize=(16, 8))
+    fig = plt.figure(figsize=(10, 8))
     plt.title('Loss')
     plt.xlabel('epoch')
     plt.ylabel('loss')    
+    
+    # find minimum validaiton loss
+    min_val_loss = np.min(val_loss)
+    pos = np.nonzero(val_loss == min_val_loss)[0]
+    plt.axvline(x=pos, linewidth=2, color='grey', linestyle='--')
+    plt.text(pos+1, min_val_loss+0.1, str(min_val_loss)[0:6])
+
+    # plot train and validation curve
     train_loss = np.array(train_loss)
     val_loss = np.array(val_loss)
-    plt.plot(train_loss, color='b', label='training loss', linewidth=1)
-    plt.plot(val_loss, color='r', label='validation loss', linewidth=1)
+    plt.plot(train_loss, label='Train', linewidth=2)
+    plt.plot(val_loss, label='Validation', linewidth=2)
     plt.legend()
+    
     plt.savefig(loss_dir)
 
 if __name__=="__main__":
-    loss_dir = './results/train_results_4.json'
+    args = get_args()
+    run = args.run
+
+    loss_dir = './results/train_results_{}.json'.format(run)
     with open(loss_dir, "r") as read_file:
         data = json.load(read_file)
 
     train_loss = data['train_losses']
     val_loss = data['val_losses']
     time = len(train_loss)
-    plot_loss(train_loss=train_loss, val_loss=val_loss, loss_dir='./results/loss_4.png', time=time)
+    plot_loss(train_loss=train_loss, val_loss=val_loss, loss_dir='./results/loss_{}.png'.format(run), time=time)
 
     
