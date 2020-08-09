6a32869c625c461484e688857dfe5e8a037cb6ba
Wentao Shi <wentao771@gmail.com>
2020-08-08 20:49:17 -0500

    run5

--- train.sh
+++ train.sh
@@ -7,11 +7,13 @@
 #PBS -A hpc_gcn03
 #PBS -j oe
 
+run=5
+
 module purge
 source activate graph
 cd /work/derick/siamese-monet-project/Siamese-MoNet
 
-singularity exec --nv -B /work,/project,/usr/lib64 /home/admin/singularity/pytorch-1.5.1-dockerhub-v4.simg python train.py -trained_model_dir ../trained_models/trained_model_4.pt -loss_dir ./results/train_results_4.json &> ./results/train_4.txt 2>&1
+singularity exec --nv -B /work,/project,/usr/lib64 /home/admin/singularity/pytorch-1.5.1-dockerhub-v4.simg python train.py -trained_model_dir ../trained_models/trained_model_${run}.pt -loss_dir ./results/train_results_${run}.json &> ./results/train_${run}.txt 2>&1
 
 
 
