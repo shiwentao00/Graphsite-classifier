9b5f9a910cafee551a8cc1efb220593b91df539f
Wentao Shi <wentao771@gmail.com>
2020-08-11 22:42:06 -0500

    visulize node feature distributions.

---
+++ node_feature_norm.py
@@ -0,0 +1,59 @@
+"""
+Compute the mean and std of each feature of the entire dataset.
+"""
+import random
+import os
+import numpy as np
+from dataloader import read_cluster_file, select_classes, pocket_loader_gen
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+if __name__ == "__main__":
+    random.seed(666) # deterministic sampled pockets and pairs from dataset
+    cluster_file_dir = '../data/googlenet-classes'
+    pocket_dir = '../data/googlenet-dataset/'
+    pop_dir = '../data/pops-googlenet/'
+
+    batch_size = 4    
+    num_workers = os.cpu_count()
+    num_workers = int(min(batch_size, num_workers))
+
+    num_classes = 60
+    cluster_th = 10000 # threshold of number of pockets in a class
+
+    clusters = read_cluster_file(cluster_file_dir)
+    clusters = select_classes(clusters, num_classes, cluster_th)
+    features_to_use = ['charge', 'hydrophobicity', 'binding_probability', 'distance_to_center', 'sequence_entropy'] 
+    
+    # all pockets are here
+    data_loader = pocket_loader_gen(pocket_dir=pocket_dir, 
+                                     pop_dir=pop_dir,
+                                     clusters=clusters, 
+                                     features_to_use=features_to_use, 
+                                     batch_size=batch_size, 
+                                     shuffle=False, 
+                                     num_workers=num_workers)
+
+    # node array of entire dataset
+    nodes = []
+    for cnt, data in enumerate(data_loader):
+        batch_nodes = data.x.detach().numpy()
+        nodes.append(batch_nodes)
+        #if cnt == 3:
+        #    break
+    nodes = np.vstack(nodes)
+    print('nodes: ')
+    print(nodes)
+    # plot distributions of features
+    num_features = nodes.shape[1]
+    for ft in range(num_features):
+        ft_vec = nodes[:,ft]
+        fig_dir = './feature_distributions/' + str(ft) + '.png'
+        sns.distplot(ft_vec, bins=20)
+        plt.savefig(fig_dir)
+
+
+
+    # compute mean and std for each feature/column
+
