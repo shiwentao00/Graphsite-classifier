df29b5a3e4be8c1fe903809f3e9eb1eb705c7caa
Wentao Shi <wentao771@gmail.com>
2020-09-15 14:27:44 -0500

    run 52

--- train.py
+++ train.py
@@ -183,7 +183,8 @@ if __name__=="__main__":
     batch_size = 256
     print('batch size:', batch_size)
     learning_rate = 0.003
-    weight_decay = 0.0005
+    #weight_decay = 0.0005
+    weight_decay = 0.001
     normalize = True # whether to normalize the embeddings in constrastive loss
     
     # margins for the relaxed contrastive loss
--- train.sh
+++ train.sh
@@ -5,7 +5,7 @@
 #PBS -A hpc_michal01
 #PBS -j oe
 
-run=51
+run=52
 
 module purge
 source activate graph
