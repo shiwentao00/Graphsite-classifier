e307f13f8af9dd96518a3eba3e3419fa181f1e2c
Wentao Shi <wentao771@gmail.com>
2020-09-24 21:47:02 -0500

    results of classifier with CrossEntropy and Focal loss

---
+++ download_classifier_results.sh
@@ -0,0 +1,13 @@
+run=2
+
+mkdir ./results/classifier_run_${run}/
+
+cd ./results/classifier_run_${run}/
+
+rsync -a derick@smic.hpc.lsu.edu:/work/derick/siamese-monet-project/Siamese-MoNet/results/train_classifier*${run}* ./
+
+rsync -a derick@smic.hpc.lsu.edu:/work/derick/siamese-monet-project/trained_models/trained_classifier_model_${run}.pt ../../../trained_models/
+
+cd ../../
+
+
