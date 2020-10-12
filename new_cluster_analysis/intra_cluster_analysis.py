250d043076c69a39dfdd7587eb0213c38a808765
Wentao Shi <wentao771@gmail.com>
2020-10-12 18:16:32 -0500

    all clusters

--- new_cluster_analysis/intra_cluster_analysis.py
+++ new_cluster_analysis/intra_cluster_analysis.py
@@ -7,6 +7,7 @@ from itertools import combinations
 import pandas as pd
 from multiprocessing import Pool
 import time
+from tqdm import tqdm
 
 
 def get_args():
@@ -42,7 +43,7 @@ def accumulate_sim(combs):
 
 if __name__ == "__main__":
     args = get_args()
-    cluster_num = int(args.cluster)
+    #cluster_num = int(args.cluster)
     parallel = int(args.parallel)
     chunk_size = int(args.chunk_size)
     #chunk_size = 5
@@ -51,7 +52,6 @@ if __name__ == "__main__":
     with open(cluster_file_dir) as file:
         clusters = yaml.full_load(file) 
     clusters = clusters[0:30]
-    cluster = clusters[cluster_num]
     print('lengths of clusters:')
     print([len(x) for x in clusters])
 
@@ -62,36 +62,41 @@ if __name__ == "__main__":
     sim = dict(zip(sim.pair, sim.value))
     toc = time.perf_counter()
     print(f"Loaded all similarities in {toc - tic:0.4f} seconds")
-    #print(sim['2q7gA01-3ze8A02'])
-    #print(sim['aa'])
-    #print(sim)
 
-    # combinations of all pockets in this cluster
-    combs = list(combinations(cluster, 2))
-    num_comb = len(combs)
-    print('number of combinations: ', num_comb)
+    for cluster_num in range(0, 30):
+        print('processing cluster {}...'.format(cluster_num))
+        cluster = clusters[cluster_num]
 
-    """
-    # divide the combination list into list of lists
-    comb_chunks = [comb[x:x+chunk_size] for x in range(0, num_comb, chunk_size)]
-    print('length of chunks: ')
-    print([len(x) for x in comb_chunks])
+        # combinations of all pockets in this cluster
+        combs = list(combinations(cluster, 2))
+        num_comb = len(combs)
+        print('number of combinations: ', num_comb)
 
-    # use multiprocessing to process chunks in parallel
-    with Pool(processes=parallel) as pool:
-        result = pool.map(accumulate_sim, comb_chunks)
+        cluster_similarity = 0
+        for comb in tqdm(combs):
+            if '{}-{}'.format(comb[0], comb[1]) in sim:
+                cluster_similarity += sim['{}-{}'.format(comb[0], comb[1])]
+            elif '{}-{}'.format(comb[1], comb[0]) in sim:
+                cluster_similarity += sim['{}-{}'.format(comb[1], comb[0])]
 
-    # sum up the similarites of each process, and average
-    cluster_similarity = sum(result)
-    """
-    cluster_similarity = 0
-    for comb in combs:
-        if '{}-{}'.format(comb[0], comb[1]) in sim:
-            cluster_similarity += sim['{}-{}'.format(comb[0], comb[1])]
-        elif '{}-{}'.format(comb[1], comb[0]) in sim:
-            cluster_similarity += sim['{}-{}'.format(comb[1], comb[0])]
+        cluster_similarity = cluster_similarity/num_comb
 
-    print('cluster similarity of cluster {}: {}'.format(cluster_num, cluster_similarity))
+        print('cluster similarity of cluster {}: {}'.format(cluster_num, cluster_similarity))
+        print('**************************************************************\n')
+
+        """
+        # divide the combination list into list of lists
+        comb_chunks = [comb[x:x+chunk_size] for x in range(0, num_comb, chunk_size)]
+        print('length of chunks: ')
+        print([len(x) for x in comb_chunks])
+
+        # use multiprocessing to process chunks in parallel
+        with Pool(processes=parallel) as pool:
+            result = pool.map(accumulate_sim, comb_chunks)
+
+        # sum up the similarites of each process, and average
+        cluster_similarity = sum(result)
+        """
 
     
 
