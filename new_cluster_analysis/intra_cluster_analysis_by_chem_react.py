aa68bcdef9ff1bc3e0695d881f6a55d2c934ebfb
Wentao Shi <wentao771@gmail.com>
2020-10-13 10:38:37 -0500

    cluster analysis based on chemical reaction

--- new_cluster_analysis/intra_cluster_analysis_by_chem_react.py
+++ new_cluster_analysis/intra_cluster_analysis_by_chem_react.py
@@ -6,7 +6,7 @@ import numpy as np
 import pandas as pd
 from tqdm import tqdm
 import time
-from itertools import combinations
+from itertools import combinations, product
 
 
 if __name__=="__main__":
@@ -20,13 +20,16 @@ if __name__=="__main__":
 
     # get similarities as ditionary
     tic = time.perf_counter()
-    #sim = pd.read_csv('../../data/googlenet-ssc.dat', sep=' ', names=['pair', 'value'], engine='python')
     sim = pd.read_csv('../../data/googlenet-ssc.dat', sep=' ', names=['pair', 'pair_b', 'value'], engine='python')
     sim['pair'] = sim.pair + '-' + sim.pair_b
     sim = dict(zip(sim.pair, sim.value))
     toc = time.perf_counter()
     print(f"Loaded all similarities in {toc - tic:0.4f} seconds")
 
+    # numpy array for similarities
+    similarity_mat = np.empty([30, 30])
+
+    # intra-cluster similarity
     for cluster_num in range(30):
         print('generating similarity matrix for cluster {}...'.format(cluster_num))
         cluster = clusters[cluster_num]
@@ -44,6 +47,33 @@ if __name__=="__main__":
 
         print('valid number of pairs: ', num_comb)
         cluster_similarity = cluster_similarity/float(num_comb+0.000000000000000001)
-
         print('cluster similarity of cluster {}: {}'.format(cluster_num, cluster_similarity))
+        similarity_mat[cluster_num][cluster_num] = cluster_similarity
         print('**************************************************************\n')
+
+    # inter-cluster similarity
+    for a in range(30):
+        for b in range(cluster_a + 1, 30):
+            cluster_a = clusters[a]
+            cluster_b = clusters[b]
+            combs = list(product(cluster_a, cluster_b))
+            
+            cluster_similarity = 0
+            num_comb = 0 # include valid combinations only
+            for comb in combs:
+                if '{}-{}'.format(comb[0], comb[1]) in sim:
+                    cluster_similarity += sim['{}-{}'.format(comb[0], comb[1])]
+                    num_comb += 1
+                elif '{}-{}'.format(comb[1], comb[0]) in sim:
+                    cluster_similarity += sim['{}-{}'.format(comb[1], comb[0])]
+                    num_comb += 1
+
+            print('valid number of pairs: ', num_comb)
+            cluster_similarity = cluster_similarity/float(num_comb+0.000000000000000001)
+            print('cluster similarity of cluster {} and {}: {}'.format(a, b, cluster_similarity))
+            similarity_mat[a][b] = cluster_similarity
+            similarity_mat[b][a] = cluster_similarity
+            print('**************************************************************\n')  
+
+    # save the matrix to plot a heatmap
+    np.save('./cluster_similarity_matrix.npy', similarity_mat)              
