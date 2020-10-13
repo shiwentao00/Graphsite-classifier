c44136167d9359aaef52f8b93e11116d03509778
Wentao Shi <wentao771@gmail.com>
2020-10-12 21:22:35 -0500

    aa

--- new_cluster_analysis/intra_cluster_analysis_by_chem_react.py
+++ new_cluster_analysis/intra_cluster_analysis_by_chem_react.py
@@ -6,29 +6,26 @@ import numpy as np
 import pandas as pd
 from tqdm import tqdm
 import time
-from itertools import combinations 
+from itertools import combinations
 
 
 if __name__=="__main__":
     # read clusters
     cluster_file_dir = "../../data/clusters_after_remove_files_with_no_popsa.yaml"
     with open(cluster_file_dir) as file:
-        clusters = yaml.full_load(file) 
+        clusters = yaml.full_load(file)
     clusters = clusters[0:30]
     print('lengths of clusters:')
     print([len(x) for x in clusters])
 
-
     # get similarities as ditionary
     tic = time.perf_counter()
-    #sim = pd.read_csv('../../data/googlenet-ssc.dat', sep=' ', names=['pair', 'value'], engine='python')
-    sim = pd.read_csv('./chem_react_test.dat', sep=' ', names=['pair', 'pair_b', 'value'], engine='python')
+    sim = pd.read_csv('../../data/googlenet-ssc.dat', sep=' ', names=['pair', 'value'], engine='python')
+    #sim = pd.read_csv('./chem_react_test.dat', sep=' ', names=['pair', 'pair_b', 'value'], engine='python')
     sim['pair'] = sim.pair + '-' + sim.pair_b
     sim = dict(zip(sim.pair, sim.value))
     toc = time.perf_counter()
     print(f"Loaded all similarities in {toc - tic:0.4f} seconds")
-
-    print(sim)
 
     for cluster_num in range(30):
         print('generating similarity matrix for cluster {}...'.format(cluster_num))
@@ -50,4 +47,3 @@ if __name__=="__main__":
 
         print('cluster similarity of cluster {}: {}'.format(cluster_num, cluster_similarity))
         print('**************************************************************\n')
-
