fc3ffab8b4bdd7bc5cf4300766c562b4cc4381f9
Wentao Shi <wentao771@gmail.com>
2020-10-29 15:05:32 -0500

    updated graph figure of pockets

--- graph_forming_figure/plot_atoms.py
+++ graph_forming_figure/plot_atoms.py
@@ -6,38 +6,44 @@ from mpl_toolkits import mplot3d
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.spatial import distance
+from matplotlib import rcParams
+import matplotlib
 
 
 if __name__ == "__main__":
+    rcParams['xtick.color'] = 'white'
+    rcParams['ytick.color'] = 'white'    
+    font = {'size': 8}
+    matplotlib.rc('font', **font)  
+
     # parse the file and read data
     atoms = open('./atoms_1.txt', 'r')
     lines = atoms.readlines()
 
-    atom_types = []
+    # plot atoms
+    fig = plt.figure(dpi=600)
+    ax = plt.axes(projection='3d')
+    color_map = {'N':'green', 'O':'blue', 'C':'red'}
+    name_map = {'N':'Nitrogen', 'O':'Oxygen', 'C':'Carbon'}
     x = []
     y = []
     z = []
-    for line in lines:
-        line = line.strip()
-        line = line.split() 
-        atom_types.append(line[1][0])
-        x.append(float(line[2]))
-        y.append(float(line[3]))
-        z.append(float(line[4]))
-        #print(line)
-
-    print(atom_types)
-    print(x)
-    print(y)
-    print(z)
-
-    # plot the atoms
-    color_map = {'N':'green', 'O':'blue', 'C':'red'}
-    colors = [color_map[x] for x in atom_types]
-    fig = plt.figure(dpi=600)
-    ax = plt.axes(projection='3d')
-    ax.scatter3D(x, y, z, c=colors, cmap='Greens')
-    ax.view_init(20, 40)
+    for atom_type in ['C', 'O', 'N']:
+        x_ = []
+        y_ = []
+        z_ = []
+        for line in lines:
+            line = line.strip()
+            line = line.split() 
+            if line[1][0] == atom_type:
+                x_.append(float(line[2]))
+                y_.append(float(line[3]))
+                z_.append(float(line[4]))
+        x.extend(x_)
+        y.extend(y_)
+        z.extend(z_)
+        ax.scatter(x_, y_, z_, c=color_map[atom_type], label=name_map[atom_type]) 
+    ax.legend(bbox_to_anchor=(0.05, 0.8), loc=2, borderaxespad=0.)
 
     # compute the pair-wise distances
     coord = np.transpose(np.vstack([x,y,z]))
@@ -45,7 +51,7 @@ if __name__ == "__main__":
     threshold_condition = atom_dist > 4.5 # set the element whose value is larger than threshold to 0
     atom_dist[threshold_condition] = 0 # set the element whose value is larger than threshold to 0
     result = np.where(atom_dist > 0)
-    print(result)
+    #print(result)
 
     # form edges
     for s, t in zip(result[0], result[1]):
@@ -59,7 +65,37 @@ if __name__ == "__main__":
 
     # plot edges 
 
+    # figure configurations
+    #ax = plt.gca()
+
+    ax.xaxis.pane.fill = False
+    ax.xaxis.pane.set_edgecolor('white')
+    ax.yaxis.pane.fill = False
+    ax.yaxis.pane.set_edgecolor('white')
+    ax.zaxis.pane.fill = False
+    ax.zaxis.pane.set_edgecolor('white')
+
+    ax.xaxis.set_ticklabels([])
+    ax.yaxis.set_ticklabels([])
+    ax.zaxis.set_ticklabels([])
+
+    #ax.set_xticks([])          # removes the ticks... great now the rest of it
+    #ax.set_yticks([])
+    #ax.set_zticks([])
+
+    ax.w_xaxis.line.set_color((1.0, 1.0, 1.0, 0.0))
+    ax.w_yaxis.line.set_color((1.0, 1.0, 1.0, 0.0))
+    ax.w_zaxis.line.set_color((1.0, 1.0, 1.0, 0.0))    
+
+    #ax.grid(False)
+    ax.grid(True)
+    #for axi in (ax.w_xaxis, ax.w_yaxis, ax.w_zaxis):
+    #    for tic in axi.get_major_ticks():
+    #        tic.tick1On = tic.tick2On = False
+    #        tic.label1On = tic.label2On = False    
+
     # save figure
+    ax.view_init(10, 35)
     plt.tight_layout()
-    plt.savefig('./atoms.png')
+    plt.savefig('./atoms.png', bbox_inches='tight')
 
