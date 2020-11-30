ea24a8f24bdd5363a07e8eb57c1efa771cf99aee
Wentao Shi <wentao771@gmail.com>
2020-11-30 11:11:07 -0600

    working on autodock vina baseline.

---
+++ baseline-vina/dock.py
@@ -0,0 +1,7 @@
+"""
+Dock each pocket with the 14 chosen ligands. The predicted class is the class
+with highest docking score.
+"""
+
+if __name__ == "__main__":
+    pass
---
+++ baseline-vina/pocket_prepare.py
@@ -0,0 +1,5 @@
+# Software tool path
+# ~/MGLTools-1.5.6/mgltools_x86_64Linux2_1.5.6/MGLToolsPckgs/AutoDockTools/Utilities24/prepare_ligand4.py
+
+if __name__ == "__main__":
+    pass
