3e0a4994d6917a6c39575d8d1b876bddaa0d9fd8
Wentao Shi <wentao771@gmail.com>
2021-12-03 14:56:23 -0600

    code clean

--- gnn/model.py
+++ gnn/model.py
@@ -12,128 +12,76 @@ import numpy as np
 from torch_geometric.nn import MessagePassing
 
 
-class SCNWMConv(GINConv):
+class GraphsiteClassifier(torch.nn.Module):
     """
-    This model implements single-channel neural weighted message. It can be
-    done by inheriting the CINConv and adding the edge neural network. 
+    Standard classifier to classify the binding sites.
     """
 
-    def __init__(self, nn, train_eps, num_features, num_edge_attr):
+    def __init__(self, num_classes, num_features, dim, train_eps,
+                 num_edge_attr, which_model, num_layers, num_channels,
+                 deg=None):
         """
-        num_features: number of features of input nodes.
+        train_eps: for the SCNWMConv module only when which_model in 
+        ['jk', 'residual', 'jknmm', and 'normal'].
+        deg: for PNAEmbeddingNet only, can not be None when which_model=='pna'.
         """
-        super(SCNWMConv, self).__init__(nn=nn, train_eps=train_eps)
-        self.edge_transformer = Sequential(Linear(num_edge_attr, 8),
-                                           LeakyReLU(),
-                                           Linear(8, 1),
-                                           ELU()
-                                           )
+        super(GraphsiteClassifier, self).__init__()
+        self.num_classes = num_classes
 
-    def forward(self, x, edge_index, edge_attr, size=None):
-        # x: OptPairTensor
-        if isinstance(x, Tensor):
-            x = (x, x)
+        # use one of the embedding net
+        if which_model == 'residual':
+            self.embedding_net = ResidualEmbeddingNet(
+                num_features=num_features,
+                dim=dim, train_eps=train_eps,
+                num_edge_attr=num_edge_attr
+            )
+        elif which_model == 'jk':
+            self.embedding_net = JKEmbeddingNet(
+                num_features=num_features,
+                dim=dim, train_eps=train_eps,
+                num_edge_attr=num_edge_attr,
+                num_layers=num_layers
+            )
+        elif which_model == 'pna':
+            self.embedding_net = PNAEmbeddingNet(
+                num_features=num_features,
+                dim=dim, num_edge_attr=num_edge_attr,
+                num_layers=num_layers, deg=deg
+            )
+        elif which_model == 'jknwm':
+            self.embedding_net = JKMCNWMEmbeddingNet(
+                num_features=num_features,
+                dim=dim, train_eps=train_eps,
+                num_edge_attr=num_edge_attr,
+                num_layers=num_layers,
+                num_channels=num_channels
+            )
+        elif which_model == 'jkgin':
+            self.embedding_net = JKEGINEmbeddingNet(
+                num_features=num_features,
+                dim=dim, train_eps=train_eps,
+                num_edge_attr=num_edge_attr,
+                num_layers=num_layers
+            )
+        else:
+            self.embedding_net = EmbeddingNet(
+                num_features=num_features,
+                dim=dim, train_eps=train_eps,
+                num_edge_attr=num_edge_attr)
 
-        # propagate_type: (x: OptPairTensor)
-        out = self.propagate(edge_index, x=x, edge_attr=edge_attr, size=size)
-
-        x_r = x[1]
-        if x_r is not None:
-            out += (1 + self.eps) * x_r
-
-        return self.nn(out)
-
-    def message(self, x_j, edge_attr):
-        weight = self.edge_transformer(edge_attr)
-
-        # message size: num_features or dim
-        # weight size: 1
-        # all the dimensions in a node share one weight generated from edge attribute
-        return x_j * weight
-
-    def __repr__(self):
-        return '{}(nn={})(edge_transformer={})'.format(
-            self.__class__.__name__,
-            self.nn,
-            self.edge_transformer
-        )
-
-
-class EmbeddingNet(torch.nn.Module):
-    def __init__(self, num_features, dim, train_eps, num_edge_attr):
-        super(EmbeddingNet, self).__init__()
-
-        self.set2set = Set2Set(
-            in_channels=dim, processing_steps=5, num_layers=2
-        )
-
-        nn1 = Sequential(Linear(num_features, dim),
-                         LeakyReLU(), Linear(dim, dim))
-        self.conv1 = SCNWMConv(nn1, train_eps, num_features, num_edge_attr)
-        self.bn1 = torch.nn.BatchNorm1d(dim)
-
-        nn2 = Sequential(Linear(dim, dim), LeakyReLU(), Linear(dim, dim))
-        self.conv2 = SCNWMConv(nn2, train_eps, dim, num_edge_attr)
-        self.bn2 = torch.nn.BatchNorm1d(dim)
-
-        nn3 = Sequential(Linear(dim, dim), LeakyReLU(), Linear(dim, dim))
-        self.conv3 = SCNWMConv(nn3, train_eps, dim, num_edge_attr)
-        self.bn3 = torch.nn.BatchNorm1d(dim)
-
-        nn4 = Sequential(Linear(dim, dim), LeakyReLU(), Linear(dim, dim))
-        self.conv4 = SCNWMConv(nn4, train_eps, dim, num_edge_attr)
-        self.bn4 = torch.nn.BatchNorm1d(dim)
-
-        #nn5 = Sequential(Linear(dim, dim), ReLU(), Linear(dim, dim))
-        #self.conv5 = GINConv(nn5)
-        #self.bn5 = torch.nn.BatchNorm1d(dim)
-
-        #nn6 = Sequential(Linear(dim, dim), ReLU(), Linear(dim, dim))
-        #self.conv6 = GINConv(nn6)
-        #self.bn6 = torch.nn.BatchNorm1d(dim)
-
-        #self.fc1 = Linear(dim, dim)
-        # self.fc2 = Linear(dim, dim) # generate embedding here
+        # set2set doubles the size of embeddnig
+        self.fc1 = Linear(2 * dim, dim)
+        self.fc2 = Linear(dim, self.num_classes)
 
     def forward(self, x, edge_index, edge_attr, batch):
-        x = F.leaky_relu(self.conv1(x, edge_index, edge_attr))
-        x = self.bn1(x)
-        x = F.leaky_relu(self.conv2(x, edge_index, edge_attr))
-        x = self.bn2(x)
-        x = F.leaky_relu(self.conv3(x, edge_index, edge_attr))
-        x = self.bn3(x)
-        x = F.leaky_relu(self.conv4(x, edge_index, edge_attr))
-        x = self.bn4(x)
+        x = self.embedding_net(x=x, edge_index=edge_index,
+                               edge_attr=edge_attr, batch=batch)
+        x = F.dropout(x, p=0.5, training=self.training)
+        x = F.leaky_relu(self.fc1(x))
+        x = F.dropout(x, p=0.5, training=self.training)
+        x = self.fc2(x)
 
-        x = self.set2set(x, batch)
-        return x
-
-
-class ResidualBlock(torch.nn.Module):
-    """
-    A residual block which has two graph neural network layers. The output and input are summed 
-    so that the module can learn identity function.
-    """
-
-    def __init__(self, num_features, dim, train_eps, num_edge_attr):
-        super(ResidualBlock, self).__init__()
-
-        self.bn1 = torch.nn.BatchNorm1d(dim)
-        nn1 = Sequential(Linear(num_features, dim),
-                         LeakyReLU(), Linear(dim, dim))
-        self.conv1 = SCNWMConv(nn1, train_eps, num_features, num_edge_attr)
-
-    def forward(self, x, edge_index, edge_attr):
-        # store the input value
-        x_skip = x
-
-        x = self.bn1(x)
-        x = F.leaky_relu(x)
-        x = self.conv1(x, edge_index, edge_attr)
-
-        # add before activation
-        x = x + x_skip
-
+        # returned tensor should be processed by a softmax layer
         return x
 
 
@@ -302,81 +250,6 @@ class PNAEmbeddingNet(torch.nn.Module):
         return x
 
 
-class NWMConv(MessagePassing):
-    """
-    The neural weighted message (NWM) layer. output of multiple instances of this
-    will produce multi-channel output. 
-    """
-
-    def __init__(self, num_edge_attr=1, train_eps=True, eps=0):
-        super(NWMConv, self).__init__(aggr='add')
-        self.edge_nn = Sequential(Linear(num_edge_attr, 8),
-                                  LeakyReLU(),
-                                  Linear(8, 1),
-                                  ELU())
-        if train_eps:
-            self.eps = torch.nn.Parameter(torch.Tensor([eps]))
-        else:
-            self.register_buffer('eps', torch.Tensor([eps]))
-
-    def forward(self, x, edge_index, edge_attr, size=None):
-        # x: OptPairTensor
-        if isinstance(x, Tensor):
-            x = (x, x)
-
-        # propagate_type: (x: OptPairTensor)
-        out = self.propagate(edge_index, x=x, edge_attr=edge_attr, size=size)
-
-        x_r = x[1]
-        if x_r is not None:
-            out += (1 + self.eps) * x_r
-
-        return out
-
-    def message(self, x_j, edge_attr):
-        weight = self.edge_nn(edge_attr)
-
-        # message size: num_features or dim
-        # weight size: 1
-        # all the dimensions in a node masked by one weight generated from edge attribute
-        return x_j * weight
-
-    def __repr__(self):
-        return '{}(edge_nn={})'.format(self.__class__.__name__, self.edge_nn)
-
-
-class MCNWMConv(torch.nn.Module):
-    """
-    Multi-channel neural weighted message module.
-    """
-
-    def __init__(self, in_dim, out_dim, num_channels,
-                 num_edge_attr=1, train_eps=True, eps=0):
-        super(MCNWMConv, self).__init__()
-        self.nn = Sequential(Linear(in_dim * num_channels,
-                                    out_dim), LeakyReLU(), Linear(out_dim, out_dim))
-        self.NMMs = ModuleList()
-
-        # add the message passing modules
-        for _ in range(num_channels):
-            self.NMMs.append(NWMConv(num_edge_attr, train_eps, eps))
-
-    def forward(self, x, edge_index, edge_attr):
-        # compute the aggregated information for each channel
-        channels = []
-        for nmm in self.NMMs:
-            channels.append(
-                nmm(x=x, edge_index=edge_index, edge_attr=edge_attr))
-
-        # concatenate output of each channel
-        x = torch.cat(channels, dim=1)
-
-        # use the neural network to shrink dimension back
-        x = self.nn(x)
-
-        return x
-
-
 class JKMCNWMEmbeddingNet(torch.nn.Module):
     """
     Jumping knowledge embedding net inspired by the paper "Representation Learning on 
@@ -487,74 +360,203 @@ class JKEGINEmbeddingNet(torch.nn.Module):
         return x
 
 
-class GraphsiteClassifier(torch.nn.Module):
-    """Standard classifier to classify the binding sites."""
+class EmbeddingNet(torch.nn.Module):
+    def __init__(self, num_features, dim, train_eps, num_edge_attr):
+        super(EmbeddingNet, self).__init__()
 
-    def __init__(self, num_classes, num_features, dim, train_eps,
-                 num_edge_attr, which_model, num_layers, num_channels,
-                 deg=None):
-        """
-        train_eps: for the SCNWMConv module only when which_model in 
-        ['jk', 'residual', 'jknmm', and 'normal'].
-        deg: for PNAEmbeddingNet only, can not be None when which_model=='pna'.
-        """
-        super(GraphsiteClassifier, self).__init__()
-        self.num_classes = num_classes
+        self.set2set = Set2Set(
+            in_channels=dim, processing_steps=5, num_layers=2
+        )
 
-        # use one of the embedding net
-        if which_model == 'residual':
-            self.embedding_net = ResidualEmbeddingNet(
-                num_features=num_features,
-                dim=dim, train_eps=train_eps,
-                num_edge_attr=num_edge_attr
-            )
-        elif which_model == 'jk':
-            self.embedding_net = JKEmbeddingNet(
-                num_features=num_features,
-                dim=dim, train_eps=train_eps,
-                num_edge_attr=num_edge_attr,
-                num_layers=num_layers
-            )
-        elif which_model == 'pna':
-            self.embedding_net = PNAEmbeddingNet(
-                num_features=num_features,
-                dim=dim, num_edge_attr=num_edge_attr,
-                num_layers=num_layers, deg=deg
-            )
-        elif which_model == 'jknwm':
-            self.embedding_net = JKMCNWMEmbeddingNet(
-                num_features=num_features,
-                dim=dim, train_eps=train_eps,
-                num_edge_attr=num_edge_attr,
-                num_layers=num_layers,
-                num_channels=num_channels
-            )
-        elif which_model == 'jkgin':
-            self.embedding_net = JKEGINEmbeddingNet(
-                num_features=num_features,
-                dim=dim, train_eps=train_eps,
-                num_edge_attr=num_edge_attr,
-                num_layers=num_layers
-            )
-        else:
-            self.embedding_net = EmbeddingNet(
-                num_features=num_features,
-                dim=dim, train_eps=train_eps,
-                num_edge_attr=num_edge_attr)
+        nn1 = Sequential(Linear(num_features, dim),
+                         LeakyReLU(), Linear(dim, dim))
+        self.conv1 = SCNWMConv(nn1, train_eps, num_features, num_edge_attr)
+        self.bn1 = torch.nn.BatchNorm1d(dim)
 
-        # set2set doubles the size of embeddnig
-        self.fc1 = Linear(2 * dim, dim)
-        self.fc2 = Linear(dim, self.num_classes)
+        nn2 = Sequential(Linear(dim, dim), LeakyReLU(), Linear(dim, dim))
+        self.conv2 = SCNWMConv(nn2, train_eps, dim, num_edge_attr)
+        self.bn2 = torch.nn.BatchNorm1d(dim)
+
+        nn3 = Sequential(Linear(dim, dim), LeakyReLU(), Linear(dim, dim))
+        self.conv3 = SCNWMConv(nn3, train_eps, dim, num_edge_attr)
+        self.bn3 = torch.nn.BatchNorm1d(dim)
+
+        nn4 = Sequential(Linear(dim, dim), LeakyReLU(), Linear(dim, dim))
+        self.conv4 = SCNWMConv(nn4, train_eps, dim, num_edge_attr)
+        self.bn4 = torch.nn.BatchNorm1d(dim)
+
+        #nn5 = Sequential(Linear(dim, dim), ReLU(), Linear(dim, dim))
+        #self.conv5 = GINConv(nn5)
+        #self.bn5 = torch.nn.BatchNorm1d(dim)
+
+        #nn6 = Sequential(Linear(dim, dim), ReLU(), Linear(dim, dim))
+        #self.conv6 = GINConv(nn6)
+        #self.bn6 = torch.nn.BatchNorm1d(dim)
+
+        #self.fc1 = Linear(dim, dim)
+        # self.fc2 = Linear(dim, dim) # generate embedding here
 
     def forward(self, x, edge_index, edge_attr, batch):
-        x = self.embedding_net(x=x, edge_index=edge_index,
-                               edge_attr=edge_attr, batch=batch)
-        x = F.dropout(x, p=0.5, training=self.training)
-        x = F.leaky_relu(self.fc1(x))
-        x = F.dropout(x, p=0.5, training=self.training)
-        x = self.fc2(x)
+        x = F.leaky_relu(self.conv1(x, edge_index, edge_attr))
+        x = self.bn1(x)
+        x = F.leaky_relu(self.conv2(x, edge_index, edge_attr))
+        x = self.bn2(x)
+        x = F.leaky_relu(self.conv3(x, edge_index, edge_attr))
+        x = self.bn3(x)
+        x = F.leaky_relu(self.conv4(x, edge_index, edge_attr))
+        x = self.bn4(x)
 
-        # returned tensor should be processed by a softmax layer
+        x = self.set2set(x, batch)
+        return x
+
+
+class SCNWMConv(GINConv):
+    """
+    This model implements single-channel neural weighted message. It can be
+    done by inheriting the GINConv and adding the edge neural network. 
+    """
+
+    def __init__(self, nn, train_eps, num_features, num_edge_attr):
+        """
+        num_features: number of features of input nodes.
+        """
+        super(SCNWMConv, self).__init__(nn=nn, train_eps=train_eps)
+        self.edge_transformer = Sequential(Linear(num_edge_attr, 8),
+                                           LeakyReLU(),
+                                           Linear(8, 1),
+                                           ELU()
+                                           )
+
+    def forward(self, x, edge_index, edge_attr, size=None):
+        # x: OptPairTensor
+        if isinstance(x, Tensor):
+            x = (x, x)
+
+        # propagate_type: (x: OptPairTensor)
+        out = self.propagate(edge_index, x=x, edge_attr=edge_attr, size=size)
+
+        x_r = x[1]
+        if x_r is not None:
+            out += (1 + self.eps) * x_r
+
+        return self.nn(out)
+
+    def message(self, x_j, edge_attr):
+        weight = self.edge_transformer(edge_attr)
+
+        # message size: num_features or dim
+        # weight size: 1
+        # all the dimensions in a node share one weight generated from edge attribute
+        return x_j * weight
+
+    def __repr__(self):
+        return '{}(nn={})(edge_transformer={})'.format(
+            self.__class__.__name__,
+            self.nn,
+            self.edge_transformer
+        )
+
+
+class ResidualBlock(torch.nn.Module):
+    """
+    A residual block which has two graph neural network layers. The output and input are summed 
+    so that the module can learn identity function.
+    """
+
+    def __init__(self, num_features, dim, train_eps, num_edge_attr):
+        super(ResidualBlock, self).__init__()
+
+        self.bn1 = torch.nn.BatchNorm1d(dim)
+        nn1 = Sequential(Linear(num_features, dim),
+                         LeakyReLU(), Linear(dim, dim))
+        self.conv1 = SCNWMConv(nn1, train_eps, num_features, num_edge_attr)
+
+    def forward(self, x, edge_index, edge_attr):
+        # store the input value
+        x_skip = x
+
+        x = self.bn1(x)
+        x = F.leaky_relu(x)
+        x = self.conv1(x, edge_index, edge_attr)
+
+        # add before activation
+        x = x + x_skip
+
+        return x
+
+
+class NWMConv(MessagePassing):
+    """
+    The neural weighted message (NWM) layer. output of multiple instances of this
+    will produce multi-channel output. 
+    """
+
+    def __init__(self, num_edge_attr=1, train_eps=True, eps=0):
+        super(NWMConv, self).__init__(aggr='add')
+        self.edge_nn = Sequential(Linear(num_edge_attr, 8),
+                                  LeakyReLU(),
+                                  Linear(8, 1),
+                                  ELU())
+        if train_eps:
+            self.eps = torch.nn.Parameter(torch.Tensor([eps]))
+        else:
+            self.register_buffer('eps', torch.Tensor([eps]))
+
+    def forward(self, x, edge_index, edge_attr, size=None):
+        # x: OptPairTensor
+        if isinstance(x, Tensor):
+            x = (x, x)
+
+        # propagate_type: (x: OptPairTensor)
+        out = self.propagate(edge_index, x=x, edge_attr=edge_attr, size=size)
+
+        x_r = x[1]
+        if x_r is not None:
+            out += (1 + self.eps) * x_r
+
+        return out
+
+    def message(self, x_j, edge_attr):
+        weight = self.edge_nn(edge_attr)
+
+        # message size: num_features or dim
+        # weight size: 1
+        # all the dimensions in a node masked by one weight generated from edge attribute
+        return x_j * weight
+
+    def __repr__(self):
+        return '{}(edge_nn={})'.format(self.__class__.__name__, self.edge_nn)
+
+
+class MCNWMConv(torch.nn.Module):
+    """
+    Multi-channel neural weighted message module.
+    """
+
+    def __init__(self, in_dim, out_dim, num_channels,
+                 num_edge_attr=1, train_eps=True, eps=0):
+        super(MCNWMConv, self).__init__()
+        self.nn = Sequential(Linear(in_dim * num_channels,
+                                    out_dim), LeakyReLU(), Linear(out_dim, out_dim))
+        self.NMMs = ModuleList()
+
+        # add the message passing modules
+        for _ in range(num_channels):
+            self.NMMs.append(NWMConv(num_edge_attr, train_eps, eps))
+
+    def forward(self, x, edge_index, edge_attr):
+        # compute the aggregated information for each channel
+        channels = []
+        for nmm in self.NMMs:
+            channels.append(
+                nmm(x=x, edge_index=edge_index, edge_attr=edge_attr))
+
+        # concatenate output of each channel
+        x = torch.cat(channels, dim=1)
+
+        # use the neural network to shrink dimension back
+        x = self.nn(x)
+
         return x
 
 
