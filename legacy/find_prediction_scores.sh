dc9a1eb77731f25f6cdbee27352f0736e15c0f0a
Wentao Shi <wentao771@gmail.com>
2021-12-03 13:53:39 -0600

    clean up repo

--- legacy/find_prediction_scores.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for i in 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56
-do
-   seed=$((i-40))
-   python find_predition_scores.py -run $i -seed $seed
-   echo "-------------------------------------------"
-done
